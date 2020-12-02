7e56f504c4e478e6628c33451b250bc5e3a4b299
test <test@PRAGMA-PC43>
2020-12-02 12:27:02 +0530

    Add project files.

---
+++ FileAndStreams/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAndStreams
+{
+       public class Employee
+    {
+        public int EmpId { get; set; }
+        public String EmpName { get; set; }
+        public int salary { get; set; }
+    }
+}
